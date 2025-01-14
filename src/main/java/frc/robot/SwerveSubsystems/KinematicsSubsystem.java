c22daaeccb31e0e1778950573ac7d9f10012a769
William <willsanders671@gmail.com>
2025-01-14 15:32:36 -0600

    Odom

--- src/main/java/frc/robot/SwerveSubsystems/KinematicsSubsystem.java
+++ src/main/java/frc/robot/SwerveSubsystems/KinematicsSubsystem.java
@@ -6,15 +6,22 @@ package frc.robot.SwerveSubsystems;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.*;
 import frc.robot.Constants;
 
 public class KinematicsSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public KinematicsSubsystem() {}
+  public KinematicsSubsystem(DriveSubsystem driveSubsystem) {
+    this.driveSubsystem = driveSubsystem;
+  }
 
-  AHRS gyro = new AHRS();
+  DriveSubsystem driveSubsystem;
+
+  AHRS gyro = new AHRS(hu);
+  double gyroAngle;
   
   
   //Whenever swerve modules are passed by a function, they will be in this order:
@@ -34,10 +41,23 @@ public class KinematicsSubsystem extends SubsystemBase {
 
   SwerveDriveKinematics kinematics = new SwerveDriveKinematics(trans);
 
-  SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, null, null);
+  SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, new Rotation2d(gyro.getYaw()), stateToPositions(states));
+  Pose2d pose;
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    gyroAngle = gyro.getYaw() / 180.0 * Math.PI;
     
+    pose = odometry.update(new Rotation2d(gyroAngle), stateToPositions(states));
+  }
+
+  public SwerveModulePosition[] stateToPositions(SwerveModuleState[] states) {
+    SwerveModulePosition[] pos = {
+      new SwerveModulePosition(states[0].speedMetersPerSecond, states[0].angle),
+      new SwerveModulePosition(states[1].speedMetersPerSecond, states[1].angle),
+      new SwerveModulePosition(states[2].speedMetersPerSecond, states[2].angle),
+      new SwerveModulePosition(states[3].speedMetersPerSecond, states[3].angle)
+    };
+    return pos;
   }
 }

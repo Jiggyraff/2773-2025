b182136330ab2b94d3609f22e399223be4307980
Jiggyraff <willsanders671@gmail.com>
2025-03-09 11:42:47 -0500

    Small stash, maybe not stable

--- src/main/java/frc/robot/Information/OdometrySubsystem.java
+++ src/main/java/frc/robot/Information/OdometrySubsystem.java
@@ -65,6 +65,7 @@ public class OdometrySubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Y", pose.getY());
 
         //This constructor is for decoration, these values make no sense
+<<<<<<< Updated upstream
         // RobotConfig config = new RobotConfig(0, 0, null, 0);
         // try{
         //   config = RobotConfig.fromGUISettings();
@@ -98,19 +99,59 @@ public class OdometrySubsystem extends SubsystemBase {
         //         },
         //         this // Reference to this subsystem to set requirements
         // );
+=======
+    //     RobotConfig config = new RobotConfig(0, 0, null, 0);
+    //     try{
+    //       config = RobotConfig.fromGUISettings();
+    //     } catch (Exception e) {
+    //       // Handle exception as needed
+    //       e.printStackTrace();
+    //     }
+
+    //     // Configure AutoBuilder last
+    //     AutoBuilder.configure(
+
+    //             this::getPose, // Robot pose supplier
+    //             this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+    //             this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+    //             (speeds, feedforwards) -> driveSub.chassisDrive(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+    //             new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+    //                     new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+    //                     new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+    //             ),
+    //             config, // The robot configuration
+    //             () -> {
+    //               // Boolean supplier that controls when the path will be mirrored for the red alliance
+    //               // This will flip the path being followed to the red side of the field.
+    //               // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+    //               var alliance = DriverStation.getAlliance();
+    //               if (alliance.isPresent()) {
+    //                 return alliance.get() == DriverStation.Alliance.Red;
+    //               }
+    //               return false;
+    //             },
+    //             this // Reference to this subsystem to set requirements
+    //     );
+>>>>>>> Stashed changes
     }
     
     // double oldY;
     // double oldX;
+<<<<<<< Updated upstream
     StructArrayPublisher<SwerveModuleState> publisher = NetworkTableInstance.getDefault()
     .getStructArrayTopic("MyStates", SwerveModuleState.struct).publish();
     
+=======
+    // StructArrayPublisher<SwerveModuleState> publisher = NetworkTableInstance.getDefault()
+    // .getStructArrayTopic("MyStates", SwerveModuleState.struct).publish();
+>>>>>>> Stashed changes
     @Override
     public void periodic() {
         Rotation2d gyroAngle = new Rotation2d(gyro.getAngle() * Math.PI / 180);
         pose = m_odometry.update(gyroAngle,
         driveSub.getPositions());
-        publisher.set(driveSub.getStates());
+        // publisher.set(driveSub.getStates());
         
         
         // System.out.println(driveSub.getStates());
